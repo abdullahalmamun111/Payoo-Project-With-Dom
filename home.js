dc07ff193db99405a0190ebdab7ddf988aae1e34
unknown <almamun602767@gmail.com>
2024-09-19 07:35:41 +0600

    amount and pin functionaly added

--- home.js
+++ home.js
@@ -1,1 +1,9 @@
-console.log('home added')
+document.getElementById('add-money').addEventListener('click', function(e){
+    e.preventDefault();
+
+const addMoney = document.getElementById('amount').value ;
+console.log(addMoney);
+
+const pinNumber = document.getElementById('pinNum').value;
+console.log(pinNumber);
+})
